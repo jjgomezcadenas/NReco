bf4f5b3e6a93f6ff6251cc2494bf03f47c4908b4
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2021-08-07 00:59:00 +0200

    Add test to in_range

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,11 @@ using NReco
 using Test
 
 @testset "NReco.jl" begin
-    # Write your tests here.
+    x = collect(1:100)
+    y = collect(6:9)
+    xr = NReco.in_range(x, 5, 10) # interval is [ )
+    println(xr)
+    println(y)
+    @test all(y .== xr)
+
 end
