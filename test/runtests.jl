e7c6f3b3d722e51df5313cf84df96654d8b5028a
andLaing <a.b.r.laing@gmail.com>
2022-02-16 16:38:27 +0100

    Update tests to account for new output variables

--- test/runtests.jl
+++ test/runtests.jl
@@ -93,7 +93,7 @@ end
                 :phistd1, :zstd1, :widz1, :widphi1, :corrzphi1,
                 :phistd2, :zstd2, :widz2, :widphi2, :corrzphi2,
 			    :xs, :ys, :zs, :ux, :uy, :uz, :xt1, :yt1, :zt1,
-                :t1, :xt2, :yt2, :zt2, :t2, :x1, :y1, :z1,
+                :ti1, :t1, :xt2, :yt2, :zt2, :ti2, :t2, :x1, :y1, :z1,
                 :x2, :y2, :z2, :xr1, :yr1, :zr1, :tr1,
                 :xr2, :yr2, :zr2, :tr2, :xb1, :yb1, :zb1, :ta1,
 			    :xb2, :yb2, :zb2, :ta2]
